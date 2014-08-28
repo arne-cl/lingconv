e873c28e105ebbd034bf8c504c8bbddd169cdb9e
Arne Neumann <github@arne.cl>
2014-08-28 17:39:15 +0200

    EMERGENCY COMMIT: trying regex for one-sentence-per-line mode

--- toktxt2owpl.py
+++ toktxt2owpl.py
@@ -11,7 +11,7 @@ import os
 import argparse
 
 
-def read_tokenized_file(input_file):
+def parse_tokenized_file(input_file, split_sentences_only=False):
     # sentence endings in tokenized text, e.g. ' . ' or ' ! '
     SENTENCE_SPLIT_REGEX = re.compile(' (\.|!|\?) ')
 
@@ -19,32 +19,24 @@ def read_tokenized_file(input_file):
     # if that's not the case, simply split after one whitespace
     WORD_SPLIT_REGEX = re.compile(' \xc2\xad | ')
 
-    # all sentence endings are signalled by '\n\n'
-    text_with_newlines = re.sub(SENTENCE_SPLIT_REGEX, r' \1\n', input_file.read())
-    #return text_with_newlines
-    return re.split(WORD_SPLIT_REGEX, text_with_newlines)
-
-
-def write_owpl(lines, output_file):
-    """
-    Writes the tokenized input to an one-word-per-line output file
-    (with sentences separated by two newlines).
-
-    Params
-    ------
-    lines : list of str
-        list of strings which contain one word each
-    """
-    for line in lines:
-        output_file.write(line + '\n')
+    # produce one sentence per line
+    if split_sentences_only:
+        SENTENCE_SPLIT_REGEX = re.compile('(.*?(\.|!|\?) )')
+        return re.split(SENTENCE_SPLIT_REGEX, input_file.read())
+    # produce one word per line, with sentences separated by an empty line
+    else:
+        # all sentence endings are signalled by '\n\n'
+        text_with_newlines = re.sub(SENTENCE_SPLIT_REGEX, r' \1\n\n', input_file.read())
+        return re.split(WORD_SPLIT_REGEX, text_with_newlines)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', type=file, default=sys.stdin)
-    parser.add_argument('output_file', nargs='?', type=file, default=sys.stdout)
+    parser.add_argument('-s', '--split-only-sentences', action='store_true')
+    parser.add_argument('input_file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('output_file', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     args = parser.parse_args(sys.argv[1:])
 
-    lines = read_tokenized_file(args.input_file)
-    write_owpl(lines, args.output_file)
+    for line in parse_tokenized_file(args.input_file, args.split_only_sentences):
+        args.output_file.write(line + '\n')
 

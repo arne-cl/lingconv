3ccac8fbe923c5b0f3f748ed030d1cd861b72717
Arne Neumann <github@arne.cl>
2014-09-02 10:51:31 +0200

    tokenized-to-one-word-per-line: added conll-x and conll2009 output options

--- toktxt2owpl.py
+++ toktxt2owpl.py
@@ -46,23 +46,47 @@ def one_sentence_per_line(input_string, segmentation_marker='\n\n'):
     return (line.strip() for line in marked_sents_str.splitlines())
 
 
-def one_word_per_line(input_string, segmentation_marker='\n\n'):
+def one_word_per_line(input_string, segmentation_marker='\n\n',
+                      output_format='owpl'):
     """
     produces a list of strings, where each string contains a single word.
     There'll be one additional string containing the segmentation marker after
     each sentence.
     """
+    assert output_format in ('owpl', 'conll2009', 'conllx')
     marked_paras_str = split_paragraphs(input_string, segmentation_marker)
     marked_sents_str = split_sentences(marked_paras_str, segmentation_marker)
     # produce one word per line, with sentences separated by an empty line
-    return re.split(WORD_SPLIT_REGEX, marked_sents_str)
+    owpl = re.split(WORD_SPLIT_REGEX, marked_sents_str)
+    output = []
+    if output_format == 'owpl':
+        output = owpl
+    else:  # output_format in ('conll2009', 'conllx'):
+        if output_format == 'conll2009':
+            num_empty_cols = 13
+        elif output_format == 'conllx':
+            num_empty_cols = 8
+        
+        output = []
+        word_id = 1
+        for line in owpl:
+            word = line.strip()
+            if word:
+                output.append("{}\t{}{}".format(word_id, word,
+                                              '\t_'*num_empty_cols))
+                word_id += 1
+            else:
+                output.append("\n")
+                word_id = 1
+    return output
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--output_format', default='owpl',
-                        help=('owpl (one-word-per-line) or '
-                              'ospl (one-sentence-per-line).'
+                        help=('owpl (one-word-per-line), '
+                              'ospl (one-sentence-per-line),'
+                              'conll2009 or conllx.'
                               'default: owpl'))
     parser.add_argument('-s', '--segmentation_marker', default='\n\n',
                         help=("The segmentation marker is added after each "
@@ -75,9 +99,10 @@ if __name__ == '__main__':
 
     input_string = args.input_file.read()
 
-    if args.output_format in ('w', 'owpl', 'one-word-per-line'):
-        lines = one_word_per_line(input_string, args.segmentation_marker)
-    elif args.output_format in ('s', 'ospl', 'one-sentence-per-line'):
+    if args.output_format in ('owpl', 'conll2009', 'conllx'):
+        lines = one_word_per_line(input_string, args.segmentation_marker,
+                                  args.output_format)
+    elif args.output_format == 'ospl':
         lines = one_sentence_per_line(input_string,
                                       args.segmentation_marker)
 
